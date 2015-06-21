6a5ba2440668bb25e98fbfe52ad123dd6fa9a60b
Jared Stewart <stewart.jared@gmail.com>
2015-06-21 15:05:01 -0500

    adding tagsoup

--- src/main/groovy/com/jaredjstewart/tree/buildTree.groovy
+++ src/main/groovy/com/jaredjstewart/tree/buildTree.groovy
@@ -1,4 +1,5 @@
 package com.jaredjstewart.tree
+import org.ccil.cowan.tagsoup.Parser
 
 def employees = [new MyObject(id: 'a'),
                  new MyObject(id: 'b', parentId: 'a'),
@@ -10,3 +11,19 @@ def employees = [new MyObject(id: 'a'),
 def tree = TreeBuilder.buildTree(employees)
 
 
+
+String ENCODING = "UTF-8"
+
+//def PARSER = new XmlSlurper(new Parser() )
+def url = "http://internalapps/sharepoint/employees/SearchAction.cfm?sort=title"
+//
+//println url.toString()
+//
+//
+//GPathResult document
+//
+//new URL(url).withReader (ENCODING) { reader ->
+//     document = PARSER.parse(reader)
+//}
+//
+//println document.'**'.findAll ({ it.@class == 'results-table' }).size()
